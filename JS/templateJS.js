56262204617d27f8f7683a786145592c885c145c
waveboo <li1322720362@gmail.com>
2018-07-23 17:52:42 +0800

    LingyuDigital Pure HTML+CSS

---
+++ JS/templateJS.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Wavelee on 2018/7/9.
+ */
+
+$(document).ready(function(){
+    $("#head-template").load("headTemplate.html");
+    $("#tail-template").load("tailTemplate.html");
+});
